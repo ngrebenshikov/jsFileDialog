164a67bec14dc9bd086d1274d8e2ad977914e4d9
Viktori <ViktoriLisenok@yandex.ru>
2017-03-03 09:10:34 +0700

    FileDialog is not static now.

--- FileDialog.hx
+++ FileDialog.hx
@@ -3,24 +3,27 @@ package ;
 import haxe.io.Bytes;
 import js.html.FileReader;
 import js.html.InputElement;
-import js.JQuery;
+import js.jquery.JQuery;
 
 class FileDialog {
 
-    private static var INPUT_ID = "haxe-js-file-dialog-input";
+    private var INPUT_ID = "haxe-js-file-dialog-input";
+    private var input = null;
 
-    private static var inputElement(get, never): InputElement;
-    private static function get_inputElement(): InputElement {
+    private var inputElement(get, never): InputElement;
+    private function get_inputElement(): InputElement {
         return cast new JQuery("#"+INPUT_ID).get(0);
     }
 
-    public static function init(acceptFileTypes: Array<String> = null, multiple: Bool = false) {
+    public function new(inputId: String, acceptFileTypes: Array<String> = null, multiple: Bool = false) {
         new JQuery("").ready(function(e) {
+            INPUT_ID += inputId;
             var accept: String;
             if (acceptFileTypes != null) {
                 accept = acceptFileTypes.toString();
             } else accept = "";
-            var input = new JQuery("<input id='" + INPUT_ID + "' type='file' accept='" + accept + "' style='display:none' ></input>");
+            if (input == null) input = new JQuery("<input id='" + INPUT_ID + "' type='file' accept='" + accept + "' style='display:none' ></input>");
+            else input.attr('accept', accept);
             input.appendTo("body");
             input.change(function(e) {
                 var f = inputElement.files[0];
@@ -35,10 +38,14 @@ class FileDialog {
         });
     }
 
-    public static function open() {
+    public function open() {
         new JQuery("#"+INPUT_ID).click();
     }
 
-    public static dynamic function onLoadEnd(name: String, type: String, bytes: Bytes) {}
+    public function dispose() {
+        new JQuery("#"+INPUT_ID).remove();
+    }
+
+    public dynamic function onLoadEnd(name: String, type: String, bytes: Bytes) {}
 
 }
